87c3afebd54e8715784750b4108886b9e77e6fe1
fsaal <f.saalfrank@gmail.com>
2024-01-22 19:57:07 +0100

    Add total reps and weight to info cards

--- src/pages/powerlifting_statistics.py
+++ src/pages/powerlifting_statistics.py
@@ -8,10 +8,18 @@ def powerlifting_content(df):
     """
     This function returns a Dash component containing a scatter plot of powerlifting exercises.
     """
-    powerlifting_exercises = ["Barbell Squat", "Barbell Bench Press", "Barbell Deadlift"]
+    powerlifting_exercises = [
+        "Barbell Squat",
+        "Barbell Bench Press",
+        "Barbell Deadlift",
+    ]
     df_powerlifting = df[df["Exercise Name"].isin(powerlifting_exercises)]
     fig = px.scatter(
-        df_powerlifting, y="Weight", color="Exercise Name", symbol="Exercise Name", hover_name="Repetitions"
+        df_powerlifting,
+        y="Weight",
+        color="Exercise Name",
+        symbol="Exercise Name",
+        hover_name="Repetitions",
     )
     fig.update_xaxes(title_text="Time")
     fig.update_yaxes(title_text="Weight [kg]")
@@ -21,10 +29,24 @@ def powerlifting_content(df):
     exercise_icons = []
     for exercise in powerlifting_exercises:
         df_exercise = df[df["Exercise Name"] == exercise]
+        # Information about PR
         record_idx = df_exercise["Weight"].argmax()
         record_date = df_exercise.index[record_idx].strftime("%d.%m.%y")
         record = df_exercise["Weight"][record_idx]
-        strength_cards.append(strength_card(exercise.split(" ")[1], record, record_date, "pajamas:weight"))
+
+        total_reps = df_exercise["Repetitions"].sum()
+        # Total weight in tonnes
+        total_weight = (df_exercise["Weight"] * df_exercise["Repetitions"]).sum() / 1000
+        strength_cards.append(
+            strength_card(
+                exercise.split(" ")[1],
+                record,
+                record_date,
+                total_reps,
+                total_weight,
+                "pajamas:weight",
+            )
+        )
     strength_cards = dmc.Group(strength_cards, spacing="md")
 
     return dmc.Paper([dcc.Graph(figure=fig), strength_cards])
@@ -34,7 +56,7 @@ def calculate_wilks_score():
     return None
 
 
-def strength_card(exercise, weight, date, icon, icon_size=30):
+def strength_card(exercise, weight, date, total_reps, total_weight, icon, icon_size=30):
     return dmc.Card(
         children=[
             dmc.Stack(
@@ -49,7 +71,15 @@ def strength_card(exercise, weight, date, icon, icon_size=30):
                     dmc.Group(
                         [
                             dmc.Text(children=weight, weight=500, size="xl"),
-                            dmc.Text(children=date, color="dimmed", size="sm", align="right"),
+                            dmc.Text(
+                                children=date, color="dimmed", size="sm", align="right"
+                            ),
+                        ]
+                    ),
+                    dmc.Group(
+                        [
+                            dmc.Text(children=f"Reps: {total_reps}"),
+                            dmc.Text(children=f"Weight: {total_weight:.0f} t"),
                         ]
                     ),
                 ],
