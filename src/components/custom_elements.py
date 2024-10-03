a06ceef85e19946edaa06286e80435c97a719d6e
FSaal <f.saalfrank@gmail.com>
2024-10-03 11:31:16 +0200

    Add segmented control with title and description

--- src/components/custom_elements.py
+++ src/components/custom_elements.py
@@ -18,3 +18,16 @@ def create_switch_card(switch_id, label, description):
         withBorder=True,
         padding=7,
     )
+
+
+def segmented_control(title=None, description=None, **kwargs):
+    """Super segmentedControl element, with added title and description ability (similar to select)."""
+    layout = dmc.Stack(
+        [
+            dmc.Text(title, fw=475, size="sm") if title else None,
+            dmc.Text(description, c="dimmed", size="xs") if description else None,
+            dmc.SegmentedControl(**kwargs),
+        ],
+        gap=1,
+    )
+    return layout
