7fc2dd71a903a3d2bf157f55aeceb752e0e89a1d
FSaal <f.saalfrank@gmail.com>
2024-10-03 12:14:55 +0200

    Improve layout, make sunburst selectable

--- src/pages/program_statistics.py
+++ src/pages/program_statistics.py
@@ -3,64 +3,99 @@ import plotly.express as px
 from dash import Input, Output, State, dcc
 from dash.exceptions import PreventUpdate
 
+from src.components.custom_elements import segmented_control
+
 
 def program_layout(app, df):
     get_callbacks(app, df)
     # fig_exercise_sunburst = plot_exercise_sunburst(df)
-    layout = dmc.Stack(
+    layout = dmc.Grid(
         [
-            # dmc.Paper(dcc.Graph(figure=fig_exercise_sunburst), shadow="md"),
-            dmc.Select(
-                id="dropdown-names",
-                data=["Muscle Category", "Exercise Type", "Exercise Name"],
-                value="Muscle Category",
-                # label="Muscle Group",
-                # description="Filter exercises by muscle group",
+            dmc.GridCol(
+                dmc.Stack(
+                    [
+                        dmc.Card(
+                            [
+                                dmc.Stack(
+                                    [
+                                        dmc.Text("Pie chart (Left)"),
+                                        dmc.Select(
+                                            id="dropdown-names",
+                                            data=["Muscle Category", "Exercise Type"],
+                                            value="Muscle Category",
+                                            label="Category",
+                                            description="Splits the chart into different categories",
+                                        ),
+                                        dmc.Select(
+                                            id="dropdown-values",
+                                            data=["Repetitions", "Weight", "Volume"],
+                                            value="Repetitions",
+                                            label="Muscle Group",
+                                            description="Size the pieces",
+                                        ),
+                                    ]
+                                )
+                            ],
+                            withBorder=True,
+                        ),
+                        dmc.Card(
+                            dmc.Stack(
+                                [
+                                    segmented_control(
+                                        title="Primary Group",
+                                        description="First-level chart organization",
+                                        id="segment-sunburst-first-path",
+                                        data=[
+                                            {"value": "Muscle Category", "label": "Muscle"},
+                                            {"value": "Exercise Type", "label": "Equipment"},
+                                        ],
+                                        value="Muscle Category",
+                                    ),
+                                    segmented_control(
+                                        title="Display metric",
+                                        description="Data shown in segment",
+                                        id="segment-sunburst-value",
+                                        data=["Repetitions", "Volume", "Weight"],
+                                        value="Repetitions",
+                                    ),
+                                ]
+                            ),
+                            withBorder=True,
+                        ),
+                    ]
+                ),
+                span=2,
             ),
-            dmc.Select(
-                id="dropdown-values",
-                data=["Repetitions", "Weight", "Volume"],
-                value="Repetitions",
-                # label="Muscle Group",
-                # description="Filter exercises by muscle group",
-            ),
-            dmc.Select(
-                id="dropdown-sunburst",
-                data=["Repetitions", "Weight", "Volume"],
-                value="Repetitions",
-                # label="Muscle Group",
-                # description="Filter exercises by muscle group",
-            ),
-            dmc.Paper(dcc.Graph(id="exercise-pie"), shadow="md"),
-            dmc.Paper(dcc.Graph(id="exercise-sunburst"), shadow="md"),
+            dmc.GridCol(dmc.Stack([dmc.Paper(dcc.Graph(id="exercise-pie"), shadow="md")]), span=5),
+            dmc.GridCol(dmc.Stack([dmc.Paper(dcc.Graph(id="exercise-sunburst"), shadow="md")]), span=5),
         ]
     )
-
     return layout
 
 
 def get_callbacks(app, df):
     @app.callback(
-        Output("exercise-sunburst", "figure"),
+        Output("exercise-pie", "figure"),
         Input("dropdown-names", "value"),
         Input("dropdown-values", "value"),
         Input("tabs", "value"),
     )
-    def udpate_sth(names, values, tab):
+    def update_pie_chart(names, values, tab):
         if tab != "program":
             raise PreventUpdate
         fig = px.pie(df, values=values, names=names, hole=0.2, title=f"{values} by {names}")
         return fig
 
     @app.callback(
-        Output("exercise-pie", "figure"),
-        Input("dropdown-sunburst", "value"),
+        Output("exercise-sunburst", "figure"),
+        Input("segment-sunburst-first-path", "value"),
+        Input("segment-sunburst-value", "value"),
     )
-    def udpate_sth(values):
-        fig = px.sunburst(df, path=["Muscle Category", "Exercise Name"], values=values, title=f"{values} by Exercise")
+    def update_sunburst_chart(first_path, values):
+        fig = px.sunburst(
+            df,
+            path=[first_path, "Exercise Name"],
+            values=values,
+            title=f"{values} by Exercise, categorized in {first_path}",
+        )
         return fig
-
-
-def plot_trained_bodypart(df):
-    # Generate a pie chart, where each section is a body part (column Muscle category)
-    px.pie(df, values="Repetitions", names="Muscle Category", title="Exercises")
